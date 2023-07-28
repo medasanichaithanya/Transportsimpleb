fda6a73af65a62845ae4fa6f1452d223e6c71bb4
medasanichaithanya1999@gmail.com <medasanichaithanya1999@gmail.com>
2023-07-28 18:33:21 +0530

    migrations

--- backend/settings.py
+++ backend/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres', 
+        'USER': 'postgres',
+        'PASSWORD': 'chaithanya',
+        'HOST': 'database-1.c1iiyw7gmitx.us-east-1.rds.amazonaws.com', 
+        'PORT': '5432',
     }
 }
 
